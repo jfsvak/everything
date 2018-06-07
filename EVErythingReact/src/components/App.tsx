88d58f39571c538167dbbae07a9064298261cc37
jfsvak <jfsvak@gmail.com>
2018-06-07 21:36:57 +0800

    Change menu button to button

--- EVErythingReact/src/components/App.tsx
+++ EVErythingReact/src/components/App.tsx
@@ -97,7 +97,7 @@ class App extends React.Component<IAppProps, IAppState>  {
                         <h1>Simple Sidebar</h1>
                         <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
                         <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
-                        <a href="#menu-toggle" onClick={this.toggleMenu} className="btn btn-secondary" id="menu-toggle">Toggle Menu</a>
+                        <button onClick={this.toggleMenu} className="btn btn-secondary" id="menu-toggle">Toggle Menu</button>
                     </div>
                 </div>
 
